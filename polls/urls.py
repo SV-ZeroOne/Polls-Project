94ce59497a61584620334d857949c2e5017a59cd
Steve Velcev <steve@elitehost.co.za>
2020-05-09 20:24:34 +0200

    add polls views, templates and basic functionality

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 
-
+# app_name defines the app namespace for use in templates
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
 # Naming your URL lets you refer to it unambiguously from elsewhere in Django, especially from within templates. This powerful feature allows you to make global changes to the URL patterns of your project while only touching a single file.
